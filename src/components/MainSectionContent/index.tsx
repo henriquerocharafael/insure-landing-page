efb0ad2e30cc231b97bc12e9ebb2b554ca5c9244
henriquerocharafael <rafaelrocha2500@gmail.com>
2022-12-04 21:55:49 -0300

    fixed section content icons path

--- src/components/MainSectionContent/index.tsx
+++ src/components/MainSectionContent/index.tsx
@@ -1,6 +1,9 @@
 import { Button } from "../Button";
 import { StyledMainContent } from "./styles";
 
+import iconSnappyProcess from "../../assets/icon-snappy-process.svg";
+import iconAffordablePrices from "../../assets/icon-affordable-prices.svg";
+import iconPeopleFirst from "../../assets/icon-people-first.svg";
 import bgPattern from "../../assets/bg-pattern-how-we-work-desktop.svg";
 
 export const MainSectionContent: React.FC = () => {
@@ -10,7 +13,7 @@ export const MainSectionContent: React.FC = () => {
       <h1>We're defferent</h1>
       <div className="card-container">
         <div>
-          <img src="../src/assets/icon-snappy-process.svg" alt="" />
+          <img src={iconSnappyProcess} alt="" />
           <h3>Snappy Process</h3>
           <p>
             Our application process can be completed in minutes, not hours.
@@ -18,7 +21,7 @@ export const MainSectionContent: React.FC = () => {
           </p>
         </div>
         <div>
-          <img src="../src/assets/icon-affordable-prices.svg" alt="" />
+          <img src={iconAffordablePrices} alt="" />
           <h3>Affordable Prices</h3>
           <p>
             We don't want you worrying about high monthly costs. Our prices may
@@ -26,7 +29,7 @@ export const MainSectionContent: React.FC = () => {
           </p>
         </div>
         <div>
-          <img src="../src/assets/icon-people-first.svg" alt="" />
+          <img src={iconPeopleFirst} alt="" />
           <h3>People First</h3>
           <p>
             Our plans aren't full of conditions and clauses to prevent payouts.
